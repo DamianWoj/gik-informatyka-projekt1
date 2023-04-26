3832ffe07f22b9b484c6cd323f01e9792c407b0d
milosz1503 <miloszmeller01@gmail.com>
2023-04-26 15:35:01 +0200

    WGS82, krasovsky

--- core/model/ellipse.py
+++ core/model/ellipse.py
@@ -11,9 +11,23 @@ class Ellipse:
         self.a = 6_378_137
         self.e2 = 0.00669438002290
         self.name = "GRS80"
+            
+    def set_WGS84(self):
+        self.a = 6_378_137
+        self.e2 = 1 / 298.257223563
+        self.name = "WGS84"
+
+    def set_Krasovsky(self):
+        self.a = 6378245
+        self.e2 = 1 / 298.3
+        self.name = "Krasovsky"
 
     def set_system(self, system_name):
         if system_name == "GRS80":
             self.set_GRS80()
+        elif system_name == "WGS84":
+            self.set_WGS84()
+        elif system_name == "Krasovsky":
+            self.set_Krasovsky()
         else:
             raise ValueError("Unknown system name")
